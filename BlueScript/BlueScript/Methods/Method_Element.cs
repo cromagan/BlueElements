8f5c921a25e01568fdf0650cec40e9fe3347cffe
Martin Blodig <Martin.Blodig@krones.com>
2021-04-30 09:58:32 +0200

    Neue Funktion für Script: Element

---
+++ BlueScript/BlueScript/Methods/Method_Element.cs
@@ -0,0 +1,45 @@
+﻿#region BlueElements - a collection of useful tools, database and controls
+// Authors: 
+// Christian Peter
+// 
+// Copyright (c) 2021 Christian Peter
+// https://github.com/cromagan/BlueElements
+// 
+// License: GNU Affero General Public License v3.0
+// https://github.com/cromagan/BlueElements/blob/master/LICENSE
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER  
+// DEALINGS IN THE SOFTWARE. 
+#endregion
+
+using Skript.Enums;
+using System.Collections.Generic;
+
+
+namespace BlueScript {
+    internal class Method_Element : Method {
+
+        public override string Syntax => "Element(VariableListe, Indexnummer)";
+        public override string Description => "Gibt ein das Element der Liste mit der Indexnummer als Text zurück. Die Liste beginnt mit dem Element 0.";
+        public override List<string> Comand(Script s) { return new() { "element" }; }
+        public override string StartSequence => "(";
+        public override string EndSequence => ")";
+        public override bool GetCodeBlockAfter => false;
+        public override enVariableDataType Returns => enVariableDataType.String;
+        public override List<enVariableDataType> Args => new() { enVariableDataType.Variable_List, enVariableDataType.Integer };
+        public override bool EndlessArgs => false;
+        public override strDoItFeedback DoIt(strCanDoFeedback infos, Script s) {
+            var attvar = SplitAttributeToVars(infos.AttributText, s, Args, EndlessArgs);
+            if (!string.IsNullOrEmpty(attvar.ErrorMessage)) { return strDoItFeedback.AttributFehler(this, attvar); }
+            var i = attvar.Attributes[1].ValueInt;
+            var list = attvar.Attributes[0].ValueListString;
+            if (i < 0 || i >= list.Count) { return new strDoItFeedback("Element nicht in Liste"); }
+            return new strDoItFeedback("\"" + list[i] + "\"", string.Empty);
+        }
+    }
+}
