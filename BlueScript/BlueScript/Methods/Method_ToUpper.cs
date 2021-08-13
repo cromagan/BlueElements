49b68717e439a7eceb14ddc72c56af6213115b83
Christian Peter <Christian.Peter@krones.com>
2021-08-13 13:26:16 +0200

    ToLower und ToUpper ergänzt

---
+++ BlueScript/BlueScript/Methods/Method_ToUpper.cs
@@ -0,0 +1,50 @@
+﻿// Authors:
+// Christian Peter
+//
+// Copyright (c) 2021 Christian Peter
+// https://github.com/cromagan/BlueElements
+//
+// License: GNU Affero General Public License v3.0
+// https://github.com/cromagan/BlueElements/blob/master/LICENSE
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using Skript.Enums;
+using System.Collections.Generic;
+
+namespace BlueScript {
+
+    internal class Method_ToUpper : Method {
+
+        #region Properties
+
+        public override List<enVariableDataType> Args => new() { enVariableDataType.String };
+        public override string Description => "Gibt den Text in Großbuchstaben zurück";
+        public override bool EndlessArgs => false;
+        public override string EndSequence => ")";
+        public override bool GetCodeBlockAfter => false;
+        public override enVariableDataType Returns => enVariableDataType.String;
+        public override string StartSequence => "(";
+        public override string Syntax => "ToUpper(OriginalString)";
+
+        #endregion
+
+        #region Methods
+
+        public override List<string> Comand(Script s) => new() { "toupper" };
+
+        public override strDoItFeedback DoIt(strCanDoFeedback infos, Script s) {
+            var attvar = SplitAttributeToVars(infos.AttributText, s, Args, EndlessArgs);
+            return !string.IsNullOrEmpty(attvar.ErrorMessage) ? strDoItFeedback.AttributFehler(this, attvar)
+                                                              : new strDoItFeedback(attvar.Attributes[0].ValueString.ToUpper(), enVariableDataType.String);
+        }
+
+        #endregion
+    }
+}
