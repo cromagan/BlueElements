ed114f56203a637ea9e75d957030107fc49b0ce9
Christian Peter <Christian.Peter@krones.com>
2022-03-11 12:48:11 +0100

    Bugfix

--- BlueScript/BlueScript/Methods/Method_IsType.cs
+++ BlueScript/BlueScript/Methods/Method_IsType.cs
@@ -46,32 +46,11 @@ namespace BlueScript.Methods {
         public override DoItFeedback DoIt(CanDoFeedback infos, Script s) {
             var attvar = SplitAttributeToVars(infos.AttributText, s, Args, EndlessArgs);
             if (!string.IsNullOrEmpty(attvar.ErrorMessage)) { return DoItFeedback.AttributFehler(this, attvar); }
-            switch (attvar.Attributes[1].ShortName.ToLower()) {
-                case "num":
-                    return attvar.Attributes[0] is VariableFloat ? DoItFeedback.Wahr() : DoItFeedback.Falsch();
 
-                case "str":
-                    return attvar.Attributes[0] is VariableString vs ? DoItFeedback.Wahr() : DoItFeedback.Falsch();
-
-                case "lst":
-                    return attvar.Attributes[0] is VariableListString ? DoItFeedback.Wahr() : DoItFeedback.Falsch();
-                //
-                case "dat":
-                //    if (attvar.Attributes[0].Type == enVariableDataType.Date) { return DoItFeedback.Wahr(); }
-                //    return DoItFeedback.Falsch();
-
-                case "bol":
-                    return attvar.Attributes[0].Type == VariableDataType.Bool ? DoItFeedback.Wahr() : DoItFeedback.Falsch();
-
-                case "err":
-                    return attvar.Attributes[0].Type == VariableDataType.Error ? DoItFeedback.Wahr() : DoItFeedback.Falsch();
-
-                case "ukn":
-                    return attvar.Attributes[0].Type == VariableDataType.NotDefinedYet ? DoItFeedback.Wahr() : DoItFeedback.Falsch();
-
-                default:
-                    return new DoItFeedback("Es wir als zweites Attribut ein String mit dem Inhalt num, str, lst, dat, bol, err oder ukn erwartet.");
+            if (string.Equals(attvar.Attributes[1].ReadableText, attvar.Attributes[0].ShortName, System.StringComparison.OrdinalIgnoreCase)) {
+                return DoItFeedback.Wahr();
             }
+            return DoItFeedback.Falsch();
         }
 
         #endregion
