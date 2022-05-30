49f16eaf8b862967b9f28a7ab32a8655d0bea83d
Christian Peter <cp33@gmx.de>
2022-05-30 20:08:44 +0200

    Bugfix

--- BlueScript/BlueScript/Methods/Method_isType.cs
+++ BlueScript/BlueScript/Methods/Method_isType.cs
@@ -21,38 +21,37 @@ using System.Collections.Generic;
 using BlueScript.Structures;
 using BlueScript.Variables;
 
-namespace BlueScript.Methods {
+namespace BlueScript.Methods;
 
-    internal class Method_IsType : Method {
+internal class Method_IsType : Method {
 
-        #region Properties
+    #region Properties
 
-        public override List<List<string>> Args => new() { new() { Variable.Any_Variable }, new() { VariableString.ShortName_Plain } };
-        public override string Description => "Prüft, ob der Variablenntyp dem hier angegeben Wert entspricht. Es wird keine Inhaltsprüfung ausgeführt!";
-        public override bool EndlessArgs => false;
-        public override string EndSequence => ");";
-        public override bool GetCodeBlockAfter => false;
-        public override string Returns => string.Empty;
+    public override List<List<string>> Args => new() { new() { Variable.Any_Variable }, new() { VariableString.ShortName_Plain } };
+    public override string Description => "Prüft, ob der Variablenntyp dem hier angegeben Wert entspricht. Es wird keine Inhaltsprüfung ausgeführt!";
+    public override bool EndlessArgs => false;
+    public override string EndSequence => ");";
+    public override bool GetCodeBlockAfter => false;
+    public override string Returns => string.Empty;
 
-        public override string StartSequence => "(";
-        public override string Syntax => "isType(Variable, num / str / lst / bol / err / ukn)";
+    public override string StartSequence => "(";
+    public override string Syntax => "isType(Variable, num / str / lst / bol / err / ukn)";
 
-        #endregion
+    #endregion
 
-        #region Methods
+    #region Methods
 
-        public override List<string> Comand(Script? s) => new() { "istype" };
+    public override List<string> Comand(Script? s) => new() { "istype" };
 
-        public override DoItFeedback DoIt(CanDoFeedback infos, Script s) {
-            var attvar = SplitAttributeToVars(infos.AttributText, s, Args, EndlessArgs);
-            if (!string.IsNullOrEmpty(attvar.ErrorMessage)) { return DoItFeedback.AttributFehler(this, attvar); }
+    public override DoItFeedback DoIt(CanDoFeedback infos, Script s) {
+        var attvar = SplitAttributeToVars(infos.AttributText, s, Args, EndlessArgs);
+        if (!string.IsNullOrEmpty(attvar.ErrorMessage)) { return DoItFeedback.AttributFehler(this, attvar); }
 
-            if (string.Equals(attvar.Attributes[1].ReadableText, attvar.Attributes[0].ShortName, System.StringComparison.OrdinalIgnoreCase)) {
-                return DoItFeedback.Wahr();
-            }
-            return DoItFeedback.Falsch();
+        if (string.Equals(attvar.Attributes[1].ReadableText, attvar.Attributes[0].ShortName, System.StringComparison.OrdinalIgnoreCase)) {
+            return DoItFeedback.Wahr();
         }
-
-        #endregion
+        return DoItFeedback.Falsch();
     }
+
+    #endregion
 }
