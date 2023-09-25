254c234f177288f95c38a90c53348813f176e62c
Christian Peter <Christian.Peter@krones.com>
2023-09-25 11:34:18 +0200

    Bugfix und Befehl "Ping" hinzugefügt

---
+++ BlueScript/BlueScript/Methods/Method_Ping.cs
@@ -0,0 +1,67 @@
+﻿// Authors:
+// Christian Peter
+//
+// Copyright (c) 2023 Christian Peter
+// https://github.com/cromagan/BlueElements
+//
+// License: GNU Affero General Public License v3.0
+// https://github.com/cromagan/BlueElements/blob/master/LICENSE
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#nullable enable
+
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using BlueBasics;
+using BlueScript.Enums;
+using BlueScript.Structures;
+using BlueScript.Variables;
+
+namespace BlueScript.Methods;
+
+// ReSharper disable once UnusedMember.Global
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses")]
+internal class Method_Ping : Method {
+
+    #region Properties
+
+    public override List<List<string>> Args => new() { StringVal };
+    public override string Description => "Pingt einen Server an und gibt dessen Reaktionszeit in Millsekunden zurück.\r\nTritt ein Fehler auf, für 9999 zurück gegeben.";
+    public override bool EndlessArgs => false;
+    public override string EndSequence => ")";
+    public override bool GetCodeBlockAfter => false;
+    public override MethodType MethodType => MethodType.Standard;
+    public override string Returns => VariableFloat.ShortName_Plain;
+    public override string StartSequence => "(";
+    public override string Syntax => "Ping(ServerAdresse)";
+
+    #endregion
+
+    #region Methods
+
+    public override List<string> Comand(VariableCollection? currentvariables) => new() { "ping" };
+
+    public override DoItFeedback DoIt(VariableCollection varCol, CanDoFeedback infos, ScriptProperties scp) {
+        var attvar = SplitAttributeToVars(varCol, infos.AttributText, Args, EndlessArgs, infos.Data, scp);
+        if (!string.IsNullOrEmpty(attvar.ErrorMessage)) { return DoItFeedback.AttributFehler(infos.Data, this, attvar); }
+
+        try {
+            var p = new Ping();
+            var r = p.Send(attvar.ValueStringGet(0));
+            if (r.Status == IPStatus.Success) {
+                return new DoItFeedback(r.RoundtripTime);
+            }
+        } catch { }
+
+        return new DoItFeedback(9999);
+    }
+
+    #endregion
+}
