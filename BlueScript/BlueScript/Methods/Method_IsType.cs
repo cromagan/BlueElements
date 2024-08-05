fb727ae6e1f59d0c34634d7d8eeeb4f1505e3ddc
Christian Peter <Christian.Peter@krones.com>
2024-08-05 13:12:10 +0200

    Bugfix

--- BlueScript/BlueScript/Methods/Method_IsType.cs
+++ BlueScript/BlueScript/Methods/Method_IsType.cs
@@ -34,22 +34,21 @@ internal class Method_IsType : Method {
 
     public override List<List<string>> Args => [[Variable.Any_Variable], StringVal];
     public override string Command => "istype";
+    public override List<string> Constants => ["NUM", "LST", "STR", "BOL", "UKN"];
     public override string Description => "Prüft, ob der Variablenntyp dem hier angegeben Wert entspricht. Es wird keine Inhaltsprüfung ausgeführt!";
-    public override int LastArgMinCount => -1;
-
     public override bool GetCodeBlockAfter => false;
+    public override int LastArgMinCount => -1;
     public override MethodType MethodType => MethodType.Standard;
     public override bool MustUseReturnValue => true;
     public override string Returns => VariableBool.ShortName_Plain;
     public override string StartSequence => "(";
-    public override string Syntax => "isType(Variable, num / str / lst / bol / err / ukn)";
+    public override string Syntax => "isType(Variable, num/str/lst/bol/ukn)";
 
     #endregion
 
     #region Methods
 
-   public override DoItFeedback DoIt(VariableCollection varCol, SplittedAttributesFeedback attvar, ScriptProperties scp, LogData ld) {
-       
+    public override DoItFeedback DoIt(VariableCollection varCol, SplittedAttributesFeedback attvar, ScriptProperties scp, LogData ld) {
         if (string.Equals(attvar.ReadableText(1), attvar.MyClassId(0), StringComparison.OrdinalIgnoreCase)) {
             return DoItFeedback.Wahr();
         }
