842f27960fad7857c65efc9fb86c1d62599b6de3
Christian Peter <Christian.Peter@krones.com>
2025-04-15 13:06:30 +0200

    Bugfix FilterTYpe

--- BlueControls/BlueControls/Forms_Database/FlexiFilterControl.cs
+++ BlueControls/BlueControls/Forms_Database/FlexiFilterControl.cs
@@ -418,7 +418,7 @@ public partial class FlexiFilterControl : GenericControlReciverSender, IHasSetti
 
 
             // Fall 5: Aufwendige Berechnung, wenn der Filter ein Ergebnis zurückliefert
-            if (!showDelFilterButton && filterSingle.FilterType != FilterType.Instr_GroßKleinEgal && filterSingle.SearchValue.Count == 1 && filterSingle.Column is { } c) {
+            if (!showDelFilterButton && filterSingle.FilterType != FilterType.Instr_GroßKleinEgal && filterSingle.FilterType != FilterType.BeginntMit && filterSingle.SearchValue.Count == 1 && filterSingle.Column is { } c) {
                 if (!filterSingle.FilterType.HasFlag(FilterType.GroßKleinEgal)) { Develop.DebugPrint("Falscher Filtertyp"); }
                 using var fc = new FilterCollection(filterSingle, "Contents Ermittlung");
                 showDelFilterButton = fc.Rows.Count > 0;
