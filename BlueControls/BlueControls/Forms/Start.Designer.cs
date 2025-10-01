bde35d555c575358c61e01a5c2518a5819076a60
Christian Peter <Christian.Peter@krones.com>
2025-10-01 13:20:14 +0200

    Erster Entwurf neues Startmenu

---
+++ BlueControls/BlueControls/Forms/Start.Designer.cs
@@ -0,0 +1,86 @@
+﻿using BlueControls.Controls;
+using System.ComponentModel;
+
+namespace BlueControls.Forms {
+    partial class Start {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                components?.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Forms = new BlueControls.Controls.ListBox();
+            this.capApp = new BlueControls.Controls.Caption();
+            this.pnlStatusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // capStatusBar
+            // 
+            this.capStatusBar.Size = new System.Drawing.Size(257, 24);
+            // 
+            // pnlStatusBar
+            // 
+            this.pnlStatusBar.Location = new System.Drawing.Point(0, 343);
+            this.pnlStatusBar.Size = new System.Drawing.Size(257, 24);
+            // 
+            // Forms
+            // 
+            this.Forms.AddAllowed = BlueControls.Enums.AddType.None;
+            this.Forms.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Forms.Location = new System.Drawing.Point(8, 32);
+            this.Forms.Name = "Forms";
+            this.Forms.Size = new System.Drawing.Size(240, 304);
+            this.Forms.TabIndex = 7;
+            this.Forms.ItemClicked += new System.EventHandler<BlueControls.EventArgs.AbstractListItemEventArgs>(this.Forms_ItemClicked);
+            // 
+            // capApp
+            // 
+            this.capApp.CausesValidation = false;
+            this.capApp.Location = new System.Drawing.Point(8, 8);
+            this.capApp.Name = "capApp";
+            this.capApp.Size = new System.Drawing.Size(208, 24);
+            this.capApp.Text = "Applikation wählen:";
+            // 
+            // Start
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(257, 367);
+            this.Controls.Add(this.capApp);
+            this.Controls.Add(this.Forms);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Start";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BeCreative! - (c) Christian Peter";
+            this.Controls.SetChildIndex(this.Forms, 0);
+            this.Controls.SetChildIndex(this.capApp, 0);
+            this.Controls.SetChildIndex(this.pnlStatusBar, 0);
+            this.pnlStatusBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected ListBox Forms;
+        protected Caption capApp;
+    }
+}
---
+++ BlueControls/BlueControls/Forms/Start.cs
@@ -0,0 +1,78 @@
+﻿// Authors:
+// Christian Peter
+//
+// Copyright (c) 2025 Christian Peter
+// https://github.com/cromagan/BlueElements
+//
+// License: GNU Affero General Public License v3.0
+// https://github.com/cromagan/BlueElements/blob/master/LICENSE
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using BlueBasics;
+using BlueBasics.Enums;
+using BlueControls.Interfaces;
+using BlueControls.ItemCollectionList;
+using System;
+using System.Reflection;
+
+#nullable enable
+
+namespace BlueControls.Forms {
+
+    public partial class Start : FormWithStatusBar {
+
+        #region Constructors
+
+        public Start() : base() {
+            InitializeComponent();
+
+            var types = Generic.GetTypesOfType<IIsStandalone>();
+
+            foreach (var thisType in types) {
+                var name = thisType.Name;
+                ImageCode i = ImageCode.Fragezeichen;
+
+                var attr = thisType.GetCustomAttribute<StandaloneInfoAttribute>();
+                if (attr != null) {
+                    name = attr.Name;
+                    i = attr.Symbol;
+                }
+
+                var p = new TextListItem(name, string.Empty, QuickImage.Get(i, 24), false, true, name) {
+                    Tag = thisType
+                };
+                Forms.ItemAdd(p);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public virtual void OpenWindow(Type t) {
+            var instance = (IIsStandalone)Activator.CreateInstance(t);
+
+            if (instance is System.Windows.Forms.Form frm) {
+                FormManager.RegisterForm(frm);
+                frm.Show();
+                Close();
+                frm.BringToFront();
+            }
+        }
+
+        private void Forms_ItemClicked(object sender, EventArgs.AbstractListItemEventArgs e) {
+            if (e.Item.Tag is not Type t) { return; }
+
+            OpenWindow(t);
+        }
+
+        #endregion
+    }
+}
